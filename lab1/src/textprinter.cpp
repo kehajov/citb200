e928312bdd26f5c60dfa0bd899ac0bfdee771b9b
Erdjan Kehayov <wildthing11000@gmail.com>
2019-04-07 13:31:50 +0300

    implement item total in invoice

--- lab1/src/textprinter.cpp
+++ lab1/src/textprinter.cpp
@@ -4,6 +4,8 @@ using std::endl;
 
 void TextPrinter::print(std::ostream &out, Invoice invoice){
     for(auto item : invoice.getItems()){
-        out << item.getQuantity() << " x " << item.getProduct().getName() << " (" << item.getProduct().getPrice() << ")" << endl;
+        out << item.getQuantity() << " x " << item.getProduct().getName() <<
+         " (" << item.getProduct().getPrice() << ")" << " = " <<
+          invoice.computeItemTotal(item) << endl;
     }
 }
